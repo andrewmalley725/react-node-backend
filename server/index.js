9addc50d475d256f417e731b553a222dedebb4f8
andrewmalley725 <andrew.alley99@gmail.com>
2022-11-02 17:58:45 -0600

    init repo

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 
 const cors = require('cors')
@@ -6,16 +7,42 @@ let app = express();
 
 var path = require("path");
 
+const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      port : 3306,
+      user : 'root',
+      password : 'Andyman72599',
+      database : 'SchoolSchedulingExample'
+    }
+});
+
 const port = 3001;
 
 app.use(cors())
 
-app.get('/api', (req, res) => {
-    let name = req.query.name;
-    let email = req.query.email;
-    res.json({
-        message: name + ' will be contacted at ' + email
-    })
+app.get('/search', (req, res) => {
+    let searchValue = req.query.value;
+    if (searchValue){
+        knex.select(knex.raw("concat(Subjects.CategoryID, ' ', Classes.ClassID) as Class_name"), 'Categories.CategoryDescription as Subject')
+                    .from('Classes')
+                    .innerJoin('Subjects', 'Subjects.SubjectID', '=', 'Classes.SubjectID')
+                    .innerJoin('Categories', 'Categories.CategoryID', '=', 'Subjects.CategoryID')
+                    .whereLike('Categories.CategoryDescription', '%' + searchValue + '%')
+                    .then(result => {
+            res.json({data: result});
+        });
+    }
+    else{
+        knex.select(knex.raw("concat(Subjects.CategoryID, ' ', Classes.ClassID) as Class_name"), 'Categories.CategoryDescription as Subject')
+                    .from('Classes')
+                    .innerJoin('Subjects', 'Subjects.SubjectID', '=', 'Classes.SubjectID')
+                    .innerJoin('Categories', 'Categories.CategoryID', '=', 'Subjects.CategoryID')
+                    .then(result => {
+            res.json({data: result});
+        });
+    }
 });
 
 app.listen(port, () => console.log('Server is running...'));
