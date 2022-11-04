bbbd69f36ab8e434db9b4e449d0dbfecac280fa5
andrewmalley725 <andrew.alley99@gmail.com>
2022-11-04 01:57:15 -0500

    added dropdown api endpoint

--- server/index.js
+++ server/index.js
@@ -24,14 +24,15 @@ app.use(cors())
 
 app.get('/search', (req, res) => {
     let value = req.query.value;
-    if (value) 
-    knex.select(knex.raw('distinct s.subjectcode as Class_Code, s.subjectname as Class_Name'))
-                .from('subjects as s')
-                .innerJoin('categories as c', 'c.categoryid', '=', 's.categoryid')
-                .whereLike('c.categorydescription', '%' + value + '%')
-                .then(result => {
-        res.json({data: result});
+    if (value) {
+        knex.select(knex.raw('distinct s.subjectcode as Class_Code, s.subjectname as Class_Name'))
+                    .from('subjects as s')
+                    .innerJoin('categories as c', 'c.categoryid', '=', 's.categoryid')
+                    .whereLike('c.categorydescription', '%' + value + '%')
+                    .then(result => {
+            res.json({data: result});
     });
+  }
 });
 
 app.get('/dropdown', (req, res) => {
